74007b0a9c49beae87f5a24f65266f68983f124c
Jacob Feldman <jacobgfeld@gmail.com>
2019-03-20 22:15:50 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,11 +1,12 @@
 class Dog
   attr_reader :name, :species, :age
   
-  @@all
+  @@all = []
   
   def initialize(name, species, age)
     @name = name
     @species = species
     @age = age
+    @@all << self
   end
 end
